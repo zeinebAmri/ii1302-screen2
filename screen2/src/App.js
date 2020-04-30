feb50b2584dc32a5e1069308a156dbf440b2d982
zeineb <zeineba@kth.se>
2020-04-30 06:29:18 +0200

    some css

--- screen2/src/App.js
+++ screen2/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-
+import './App.css'
 
 /* Edit these lines to reflect your IoT platform config. */
 
-class app extends Component{
+class App extends Component{
  
+
     constructor(props){
       super(props);
-      const databaseConfig = {
+      this.state ={
+        message: ""
+      }
+      var databaseConfig = {
         apiKey: "AIzaSyCv3rnRNfUOEBh5Ol1ZiGwXr6pD9cRotYU",
         authDomain: "msg-dlb.firebaseapp.com",
         databaseURL: "https://msg-dlb.firebaseio.com",
@@ -18,47 +22,63 @@ class app extends Component{
         appId: "1:58422300991:web:217d5889656b004a85c38a",
         measurementId: "G-B9KDZXM7JZ"
       };
-    
-      this.app = firebase.initializeApp(databaseConfig);
-      this.db = this.app.database().ref().child('msg');
-      
-      this.state ={
-        message: ""
+      if(!firebase.apps.length){
+      firebase.initializeApp(databaseConfig);
       }
+      this.db = firebase.database();
+      
+      var ref = this.db.ref('message')
+      ref.on('value', gotData, gotErr);
+      
+      function gotData(data){
+        //console.log(data.val())
+        var message = data.val();
+      /*  for (let [key,value] of Object.entries(message)){
+          console.log(` ${value}`)
+          let msg = ` ${value}`;
+          //return msg;
+        }*/
+        var m= Object.values(message);
+        console.log(m)
+
+   
+      }
+      function gotErr(err){
+        console.log("error")
+        console.log(err)
+      }
+
+      
+      
+      /*componentWillMount(){
+      setInterval(() => {
+      device.Push('getMessage');
+      var msg = device.getCurrentMessage()
+      this.setState({
+      message: msg
+      })
+      }, 5000);
+      */
+
+
     }
-    componentDidMount(){
-      this.db.on('message', send=>{
-        this.setState({
-          message: send.val()
-        })
-      });
-/*componentWillMount(){
-setInterval(() => {
-  device.Push('getMessage');
-  var msg = device.getCurrentMessage()
-  this.setState({
-    message: msg
-  })
-}, 5000);
-*/
-}
 render(){
   return(
-    <React.Fragment>
     <div className="App">
       <header className="App-header">
-      <p>display</p>
+            <h2>Display</h2>      
         <div className="screen">
         
-        <div id="text">{this.state.message}</div>
+        <div id="text">lol</div>
+        
         </div>
-      </header>
+        </header>
     </div>
-    </React.Fragment>
+
   )
 }
 }
 
-export default app;
+export default App;
 
 
